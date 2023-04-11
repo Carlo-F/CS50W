a6be3817434856b275e5915b6254ea96de0967ad
Carlo Feniello <carlofeniello@hotmail.it>
2023-04-11 16:03:33 +0200

    add listing,comment and bid models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    image_url = models.CharField(max_length=256)
+    category = models.CharField(max_length=64,blank=True,null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    is_active = models.BooleanField(default=1)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    listing = models.ForeignKey(Listing,on_delete=models.CASCADE, related_name="listing")
+
+    def __str__(self):
+        return f"{self.author} commented on {self.listing}"
+
+class Bid(models.Model):
+    amount = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid_author")
+    listing = models.ForeignKey(Listing,on_delete=models.CASCADE, related_name="bid_listing")
+
+    def __str__(self):
+        return f"{self.author} offered {self.amount} euros on {self.listing}"
