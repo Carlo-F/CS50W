319c70f9f602e552fc47a294aaa6095cb7321015
Carlo Feniello <carlofeniello@hotmail.it>
2023-04-18 15:17:29 +0200

    create listing page

--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,25 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User,Listing,Bid
+from .models import User,Listing
+
+class NewListingForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+    listing_title = forms.CharField(label="title")
+    listing_description = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}))
+    listing_starting_bid = forms.IntegerField(min_value=1, required=True, label="starting bid")
+    listing_image_url = forms.CharField(label="image url")
+    listing_category = forms.CharField(label="category")
 
 
 def index(request):
@@ -22,6 +37,35 @@ def index(request):
     return render(request, "auctions/index.html", {
         "active_listings": active_listings
     })
+
+@login_required
+def new_listing(request):
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+
+        if form.is_valid():
+            listing_title = form.cleaned_data['listing_title']
+            listing_description = form.cleaned_data['listing_description']
+            listing_starting_bid = form.cleaned_data['listing_starting_bid']
+            listing_image_url = form.cleaned_data['listing_image_url']
+            listing_category = form.cleaned_data['listing_category']
+
+            new_listing = Listing(
+                title=listing_title,
+                description=listing_description,
+                starting_bid=listing_starting_bid,
+                image_url=listing_image_url,
+                category=listing_category,
+                owner=request.user
+            )
+            
+            new_listing.save()
+
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/new_listing.html",{
+            "form": NewListingForm()
+        })
 
 
 def login_view(request):
