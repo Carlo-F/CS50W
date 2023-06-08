595f608dda0b8dbd987e415bc4ec094eaae99b16
Carlo Feniello <carlofeniello@hotmail.it>
2023-06-08 14:23:24 +0200

    add pagination

--- network/views.py
+++ network/views.py
@@ -7,15 +7,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 from .models import User,Post,Follow
 
 
 def index(request):
     posts = Post.objects.all().order_by('-timestamp')
+    paginator = Paginator(posts, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request, "network/index.html", {
-        "posts": posts
+        "page_obj": page_obj
     })
 
 @login_required
@@ -25,8 +30,13 @@ def following(request):
 
     posts = Post.objects.filter(user__id__in=follows.values_list('following_user_id')).order_by('-timestamp')
 
+    paginator = Paginator(posts, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, "network/following.html", {
-        "posts": posts
+        "page_obj": page_obj
     })
 
 @login_required
@@ -37,10 +47,15 @@ def profile(request,username):
         followers = profile_user.followers.all()
         follows = profile_user.following.all()
         profile_user.logged_user_is_following = profile_user.followers.filter(user=request.user)
+
+        paginator = Paginator(posts, 10)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         
         return render(request, "network/profile.html", {
                 "profile_user": profile_user,
-                "posts":posts,
+                "page_obj":page_obj,
                 "followers":followers,
                 "follows":follows
             })
