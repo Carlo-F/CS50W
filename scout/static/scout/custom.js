ab0b7a25a9ab0e0cac92efb02fba8f9f1a6d123d
Carlo Feniello <carlofeniello@hotmail.it>
2023-08-09 15:34:55 +0200

    create basic template

--- scout/static/scout/custom.js
+++ scout/static/scout/custom.js
@@ -1,3 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('hello world');
 });
+
+//searchsuggestion
+let products = results = [
+  { 'nome': 'risultato1', 'slug': 'risultato-1' },
+  {'nome': 'risultato2', 'slug': 'risultato-2'},
+]
+// fetch("/products/getProductsSuggestions", {
+//     method: "get",
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+// })
+// .then((response) => response.json())
+// .then((responseData) => {
+//     products = responseData['result']
+// });
+
+const searchInput = document.querySelector("#smart-search>input[name='query']")
+const searchSuggestion = document.querySelector("#search-suggestion")
+const suggestionsList = document.querySelector("#search-suggestion>ul")
+const template = document.querySelector('#suggestion');
+
+const SHOW_SUGGESTIONS = () => {
+    if (results.length > 0) {
+        results.forEach(result => {
+            const suggestion = template.content.cloneNode(true)
+            let link = suggestion.querySelector("#suggestion-link")
+            link.setAttribute('href', `/${result.slug}`)
+            link.innerText = result.nome
+            suggestionsList.appendChild(suggestion)
+        })
+        searchSuggestion.classList.remove("d-none");
+    } else {
+        searchSuggestion.classList.add("d-none");
+    }
+}
+
+searchInput.addEventListener('input', (e) => {
+    if (e.target.value.length > 3) {
+        results = products.filter(product => product.nome.toLowerCase().indexOf(e.target.value.trim().toLowerCase()) !== -1)
+    } else {  
+        results = []
+    }
+    suggestionsList.innerHTML = ""
+    SHOW_SUGGESTIONS()
+})
--- scout/views.py
+++ scout/views.py
@@ -1,7 +1,67 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return render(request, 'scout/index.html')
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, email=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "scout/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "scout/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "scout/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "scout/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "scout/register.html")
